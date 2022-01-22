3746b69fbc4c33dd0cb5f582f7e727f51234826c
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-22 15:00:27 +0530

    Added : Program for the Check First name is valid or not

--- src/main/java/com/blz/lambda/RegexDemo.java
+++ src/main/java/com/blz/lambda/RegexDemo.java
@@ -1,8 +1,28 @@
 package com.blz.lambda;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegexDemo {
+    Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Lambdda Expression");
+        RegexDemo regexDemo = new RegexDemo();
+        System.out.println("Enter first name");
+        regexDemo.firstName();
+    }
+
+    public void firstName() {
+        String input = sc.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
     }
 }
+
